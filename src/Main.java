06ce234e288f8c901f5aad113d0e2210972faddc
Alex Trujillo <a01328426@itesm.mx>
2019-11-14 20:20:21 -0600

    uncompleted commit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import filters.Filter;
 import filters.LowerCaseFilter;
 import filters.ShiftFilter;
 import filters.SortFilter;
@@ -5,24 +6,54 @@ import filters.SortFilter;
 import inputs.LineCounter;
 import pipeline.PipeLine;
 
-import java.io.IOException;
-import java.util.Arrays;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
+
+
     public static void main(String[] args) throws IOException {
-        PipeLine<String> pipeline = new PipeLine<String>()
-                .addFilter(new LowerCaseFilter())
-                .addFilter(new ShiftFilter())
-                .addFilter(new SortFilter());
-
-        //List<String> input = Arrays.asList("Clouds are white", "Pittsburgh is beautiful");
-        //System.out.println(pipeline.run(input));
-
         LineCounter lineCounter = new LineCounter();
-        lineCounter.readFile();
-        lineCounter.printArray();
+        lineCounter.readFile("KWIC2_input.txt");
+        lineCounter.printLines();
 
+        System.out.println("Quieres quitar lineas? especificalas separadas por comas o pon 'no'\n");
+        Scanner scanner = new Scanner(System.in);
+        lineCounter.setDeleteLines(scanner);
+
+        System.out.println("Quieres quitar stopwords? y/n\n");
+        if (scanner.nextLine().equals("y")) {
+            lineCounter.deleteStopWords();
+        }
+
+        System.out.println("Orden asc o desc?\n");
+        String filter = scanner.nextLine().equals("asc") ? SortFilter.ACC : SortFilter.DEC;
+        lineCounter.setFiltersToList(filter);
+
+
+        /*
+        for (int i = 0; i < result.size(); i++) {
+            System.out.printf("%d) %s\n", i, result.get(i));
+        }
+
+        System.out.println("Quieres quitar lineas? especificalas separadas por comas o pon 'no'\n");
+        for (String s : scanner.nextLine().split(",")) {
+            try {
+                result.set(Integer.parseInt(s), null);
+            } catch (NumberFormatException err) {
+                //
+            }
+
+        }
+
+        result.removeAll(Collections.singleton(null));
+        for (int i = 0; i < result.size(); i++) {
+            System.out.printf("%d) %s\n", i, result.get(i));
+        }
+*/
 
     }
 }
--- src/inputs/LineCounter.java
+++ src/inputs/LineCounter.java
@@ -1,30 +1,78 @@
 package inputs;
 
+import filters.LowerCaseFilter;
+import filters.ShiftFilter;
+import filters.SortFilter;
+import pipeline.PipeLine;
+
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class LineCounter {
+    private ArrayList <String> originalArray;
     private ArrayList <String> filearray;
+
 
     public LineCounter() {
         filearray  = new ArrayList<String>() ;
+        originalArray  = new ArrayList<String>() ;
     }
 
-    public  void readFile() throws FileNotFoundException, IOException{
-        File file =  new File(LineCounter.class.getResource("KWIC2_input.txt").getFile());
+    public  void readFile(String filename) throws FileNotFoundException, IOException{
+        File file =  new File(LineCounter.class.getResource(filename).getFile());
         BufferedReader br = new BufferedReader(new FileReader(file));
         String st;
-        while ((st = br.readLine()) != null)
+        while ((st = br.readLine()) != null){
+            originalArray.add(st);
             filearray.add(st);
+
+        }
 
     }
 
-    public void printArray() {
+    public void printLines() {
         System.out.println(filearray.size());
+        int i = 0;
         for (String aFilearray : filearray) {
-            System.out.println(aFilearray);
+            System.out.println( i +". " + aFilearray);
+            i++;
         }
     }
 
 
+
+    public void setDeleteLines(Scanner scanner) {
+        for (String s : scanner.nextLine().split(",")) {
+            try {
+                filearray.set(Integer.parseInt(s), null);
+            } catch (NumberFormatException err) {
+                //
+            }
+        }
+
+        filearray.removeAll(Collections.singleton(null));
+    }
+
+    public void deleteStopWords() {
+        for (int i = 0; i < filearray.size(); i++) {
+            for (String w : originalArray) {
+                filearray.set(i, filearray.get(i).replace(w, ""));
+            }
+        }
+    }
+
+    public ArrayList<String> getFilearray() {
+        return filearray;
+    }
+
+    public void setFiltersToList(String filter) {
+        PipeLine<String> pipeline = new PipeLine<String>()
+                .addFilter(new LowerCaseFilter())
+                .addFilter(new ShiftFilter())
+                .addFilter(new SortFilter(filter));
+
+        filearray = pipeline.run(filearray);
+    }
 }
